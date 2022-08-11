612375384b30595ebadc6e2343b92eb7f6de60eb
oganexon <oganexon@protonmail.com>
2022-08-11 12:44:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sveltejs-kit-template-default-vlxa2g
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sveltejs-kit-template-default-vlxa2g)
---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import adapter from '@sveltejs/adapter-auto';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter(),
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		},
+    trailingSlash: "never",
+	}
+};
+
+export default config;
